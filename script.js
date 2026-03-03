fb0d24ab027b0c01a8648096bcb8735c4ffda788
Ben Hustler <ben1hustler@gmail.com>
2026-03-02 16:59:31 -0800

    added gr custom

--- script.js
+++ script.js
@@ -101,7 +101,13 @@ function customizeSite(target) {
             resume = resumeLinks["GENERAL"];
             projectsOrdering = [1, 0, 2, 3, 4, 5];
             message = "You've found a UX Designer who creates evidence-based and human-centered designs.";
-            break;            
+            break;
+        case "gr":
+            name = "Global Relay";
+            resume = resumeLinks["GENERAL"];
+            projectsOrdering = [2, 1, 0, 3, 4, 5];
+            message = "You've found a UX Designer who's passionate about creating evidence-based and human-centered designs.";
+            break;              
         default:
             resume = resumeLinks["GENERAL"];
             projectsOrdering = [0, 1, 2, 3, 4, 5];
